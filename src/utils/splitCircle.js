788e9fb92d13516c26b9716ce9957481da5c9d5b
Kevin <kvn.philippe@gmail.com>
2019-11-03 13:48:08 +0100

    Clean

--- src/utils/splitCircle.js
+++ src/utils/splitCircle.js
@@ -3,10 +3,6 @@ export const splitCircle = (circles, id, occurences) =>
     if (circle.id === id) {
       return {
         ...circle,
-        // children: [
-        //   { id: `${id}1`, children: [] },
-        //   { id: `${id}2`, children: [] }
-        // ],
         children: new Array(occurences).fill().map((child, i) => ({
           id: `${id}${i}`,
           children: []
@@ -22,28 +18,3 @@ export const splitCircle = (circles, id, occurences) =>
     }
     return circle
   })
-
-const circles = [
-  {
-    id: '1',
-    children: [
-      {
-        id: '11',
-        children: [{ id: '111', children: [] }, { id: '112', children: [] }]
-      },
-      { id: '12', children: [] }
-    ]
-  },
-  {
-    id: '2',
-    children: [
-      { id: '21', children: [] },
-      {
-        id: '22',
-        children: [{ id: '221', children: [] }, { id: '222', children: [] }]
-      }
-    ]
-  }
-]
-
-// JSON.stringify(splitCircle(circles, '21'))
